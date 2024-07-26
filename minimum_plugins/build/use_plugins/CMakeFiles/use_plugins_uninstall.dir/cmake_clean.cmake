0a487d4efeca3d69bd97c92601ed2113dbcf5982
Piyush Tailor <piyushtailor4@gmail.com>
2024-07-26 17:40:34 +0530

    all beginner packages added,readme pending

---
+++ minimum_plugins/build/use_plugins/CMakeFiles/use_plugins_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/use_plugins_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/use_plugins_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
